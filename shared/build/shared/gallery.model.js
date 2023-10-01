8a8929f3d610e582baaa4c70acd4216696ff17db
Avner Eisenberg <avner.e@pepperi.com>
2023-10-01 17:21:09 +0300

    no message

--- shared/build/shared/gallery.model.js
+++ shared/build/shared/gallery.model.js
@@ -1,9 +1,6 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CLIENT_ACTION_ON_GALLERY_CARD_CLICK = void 0;
 // **********************************************************************************************
 //                          Client & User events const
 // **********************************************************************************************
-exports.CLIENT_ACTION_ON_GALLERY_CARD_CLICK = 'OnClientGalleryCardClick';
 // **********************************************************************************************
 //# sourceMappingURL=gallery.model.js.map
